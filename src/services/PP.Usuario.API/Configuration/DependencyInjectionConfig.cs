8d46a28099445cbb311478d4c324b3aebcf98fc4
Guilherme <guilherme.pomp@gmail.com>
2021-02-07 15:15:34 -0300

    Inicio do projeto de usuarios

---
+++ src/services/PP.Usuario.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PP.Aluno.API.Configuration
+{
+    public static class DependencyInjectionConfig {
+        public static void RegisterServices(this IServiceCollection services) {
+        }
+    }
+}
